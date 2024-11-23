efcf84d19cc89dda3ee91aa49a0ff246b10251b5
CosteaDarius <dariuscostea068@gmail.com>
2024-11-23 14:37:41 +0200

    c

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Toate câmpurile trebuie completate!");
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log("form data:", formData);
+
+  form.reset();
+});
